261252410b207bf2d382fec256c9f9154540687c
Ephrem <39829776+ephrinaw@users.noreply.github.com>
2023-01-03 13:54:53 +0200

    redeploy on github pages with edited image folder

--- constants/constants.js
+++ constants/constants.js
@@ -6,7 +6,7 @@ export const projects = [
     title: "Food App",
     description:
       "This is a food ordering application made up of React components that will let the user to order food and their detail will be available in firebase database.",
-    image: "/next.svg",
+    image: "/food-app.jpg",
     tags: ["React", "Firebase"],
     source: "https://github.com/ephrinaw/food_app",
     visit: "https://www.youtube.com/watch?v=ZcvyZGVJ46c",
@@ -16,7 +16,7 @@ export const projects = [
     title: "NextJs Meetup Registration App",
     description:
       "This is a NextJs meetup registration application made up of NextJs and React components that will let the user to arrange meetup places.In the background there is MongoDb.",
-    image: "/images/next-demo.png",
+    image: "/next-demo.png",
     tags: [ "NextJs","React","MongoDB"],
     source: "https://github.com/ephrinaw/next-demo",
     visit: "https://next-demo-ephrinaw.vercel.app/",
@@ -26,7 +26,7 @@ export const projects = [
     title: "Cooking Ingredient application",
     description:
       "This is an application that summeraizes the most commonly used react hooks as I revised my studies.",
-    image: "/images/react-hooks-demo.png" ,
+    image: "/react-hooks-demo.png" ,
     tags: ["React", "JavaScript", "Firebase"],
     source: "https://github.com/ephrinaw/react-hook-review",
     visit: "https://www.youtube.com/watch?v=NdGiJmKn4pI",
@@ -36,7 +36,7 @@ export const projects = [
     title: "React Expenses",
     description:
       "Using React, Firebase  we managed to build a simple Application - The app allows users to store their expenses  by day and they can view it as a yearly summary.",
-    image: "/images/expense-demo.png",
+    image: "/expense-demo.png",
     tags: ["React", "Firebase", "Css"],
     source: "https://github.com/ephrinaw/expense-demo",
     visit: "https://ephrinaw.github.io/expense-demo/",
@@ -47,7 +47,7 @@ export const projects = [
     title: "Fetching from API demo",
     description:
       "Upon building this demo app I have learned how to fetch data from the API and display them.",
-    image: "/images/fetch-demo.png",
+    image: "/fetch-demo.png",
     tags: ["React", "JavaScript", "Firebase"],
     source: "https://google.com",
     visit: "https://www.youtube.com/watch?v=yZevs6uZ3QE",
